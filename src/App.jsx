be63d59039d5892e4f6fa6ccb106927a66d8f7dd
NoaYaakov617 <noayaa@post.bgu.ac.il>
2024-02-04 11:42:31 +0200

    seconde commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Product from "./pages/product";
+import Pricing from "./pages/pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import Form from "./components/Form";
+import City from "./components/City";
+import { CitiesProvider } from "./contexts/CitiesContext";
+
+function App() {
+  return (
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />}></Route>
+          <Route path="product" element={<Product />}></Route>
+          <Route path="pricing" element={<Pricing />}></Route>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="*" element={<PageNotFound />}></Route>
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<Navigate to="cities" replace />} />
+            <Route path="cities" element={<CityList />} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
+  );
+}
+
+export default App;
---
+++ src/components/CityList.jsx
@@ -0,0 +1,25 @@
+import Spinner from "./Spinner";
+import styles from "./CityList.module.css";
+import CityItem from "./CityItem";
+import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
+
+function CityList() {
+  const { cities, isLoading } = useCities();
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return (
+      <Message
+        message={"Add your first city by clicking city on a city in the map"}
+      />
+    );
+  return (
+    <ul className={styles.citylist}>
+      {cities.map((city) => (
+        <CityItem key={city.id} city={city} />
+      ))}
+    </ul>
+  );
+}
+
+export default CityList;
